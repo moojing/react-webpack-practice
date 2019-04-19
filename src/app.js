c4ec1e692705211f932faa34d5731d2242817d08
moojing <moojitsai@gmail.com>
2019-04-19 16:27:30 +0800

    add react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+    return <h2>Home</h2>;
+  }
+  
+  function About() {
+    return <h2>About</h2>;
+  }
+  
+  function Users() {
+    return <h2>Users</h2>;
+  }
+
+  
+
 
 class App extends Component {
   constructor() {
@@ -7,12 +23,29 @@ class App extends Component {
       title: ""
     };
   }
+
+  
   render() {
     return (
-      <div>
+       <div>
         <h1>App</h1>
-        <img src="/assets/index.png"/>
-      </div>
+        <Router>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about/">About</Link>
+                </li>
+                <li>
+                <Link to="/users/">Users</Link>
+                </li>
+            </ul>
+            <Route path="/" exact component={Index} />
+            <Route path="/about/" component={About} />
+            <Route path="/users/" component={Users} />
+        </Router>
+       </div>
     );
   }
 }
