98bc7fc541c5802a816698754bc8c667b4c5a0ae
Tsai Mujing <moojitsai@gmail.com>
2019-06-14 19:23:47 +0800

    remove surplus file

--- src/app.js
+++ src/app.js
@@ -1,20 +1,8 @@
 import React from 'react'
-import { Route,Switch ,Redirect} from 'react-router-dom'
-import SelectPayment from './js/page/SelectPayment'
-import Pay from './js/page/Pay'
-
+ 
 const App = () => (
   < >
-   
-
-     
-   <Switch>
-        <Route  path="/select_payment" exact component={SelectPayment}/>
-        <Route  path="/pay" component={Pay}/>
-        <Redirect to="/select_payment" />
-   </Switch>
-      
-   
+   <h1>App</h1>
   </>
 )
 
